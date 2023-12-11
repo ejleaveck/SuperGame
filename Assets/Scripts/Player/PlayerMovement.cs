398aad338a5cf1e5709859f1e1a820f6c27dd916
ejleaveck <ej@qtr-dev.com>
2023-12-10 19:50:15 -0500

    Player walks, runs, and jumps.

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -1,18 +1,130 @@
+using System;
 using System.Collections;
-using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.Diagnostics;
 
-public class MovementPlayer : MonoBehaviour
+public class PlayerMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
+    // Movement variables
+    float moveDirection = 0;
+    [SerializeField] private float walkSpeed = 10f;
+    [SerializeField] private float runSpeed = 20f;
+    [SerializeField] private float accelerationTime = 1f;
+
+    private float currentSpeed;
+    private float targetSpeed;
+    private float speedSmoothing;
+
+    private bool isRunning;
+
+    // Jump variables
+    bool canJump = false;
+    bool isJumping = false;
+    [SerializeField] private float jumpForce = 30f;
+    [SerializeField] private float fallForceMultiplier = 10f;
+
+    public LayerMask jumpableLayer;
+    public Transform groundCheck;
+    public float groundCheckWidth = .5f;
+    public float groundCheckHeight = .5f;
+
+    // Player state variables
+
+
+    // Input variables
+
+
+    // References to other components or objects
+    private Rigidbody2D activePlayerRB;
+    private PlayableCharacter playerCharacterChoice;
+    private SpriteRenderer characterSpriteRenderer;
+
+
     void Start()
     {
-        
+        // Instantiate references to other components or objects
+        activePlayerRB = GetComponent<Rigidbody2D>();
+        Spawner spawner = FindObjectOfType<Spawner>();
+        playerCharacterChoice = spawner.playerChoice;
+        characterSpriteRenderer = GetComponent<SpriteRenderer>();
+
+        if (activePlayerRB == null || spawner == null)
+        {
+            Debug.Log($"{nameof(PlayerMovement)} > {nameof(Update)} > activePlayerRB=\"{activePlayerRB.IsUnityNull().ToString()}\" > spawner=\"{spawner.IsUnityNull().ToString()}\".");
+        }
+        // ...
+
+
     }
 
-    // Update is called once per frame
+
     void Update()
     {
-        
+        // Get horizontal input
+        moveDirection = Input.GetAxisRaw("Horizontal");
+        FlipSpriteBasedOnMovement(moveDirection);
+
+        isRunning = Input.GetButton("Fire1");
+        targetSpeed = !Mathf.Approximately(moveDirection, 0) ? (isRunning ? runSpeed : walkSpeed) : 0f;
+        speedSmoothing = !Mathf.Approximately(moveDirection, 0) ? walkSpeed / accelerationTime : walkSpeed / (accelerationTime / 2);
+
+
+        if (Input.GetButtonDown("Jump") && canJump)
+        {
+            isJumping = true;
+        }
+
+
+
+    }
+
+    void FixedUpdate()
+    {
+        currentSpeed = Mathf.Lerp(currentSpeed, targetSpeed, speedSmoothing * Time.deltaTime);
+        //Apply the movment
+        activePlayerRB.velocity = new Vector2(moveDirection * currentSpeed, activePlayerRB.velocity.y);
+
+        // ground check
+        Collider2D[] colliders = Physics2D.OverlapBoxAll(groundCheck.position, new Vector2(groundCheckWidth, groundCheckHeight), 0f, jumpableLayer);
+        canJump = colliders.Length > 0;
+
+        if (isJumping && canJump)
+        {
+            activePlayerRB.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+            isJumping = false;
+        }
+    }
+
+    void FlipSpriteBasedOnMovement(float horizontalDirection)
+    {
+        if (horizontalDirection != 0)
+        {
+            characterSpriteRenderer.flipX = horizontalDirection < 0;
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.layer == LayerMask.NameToLayer("Jumpable"))
+        {
+            canJump = true;
+
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (((1 << collision.gameObject.layer) & jumpableLayer) != 0)
+        {
+            canJump = false;
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireCube(groundCheck.position, new Vector3(groundCheckWidth, groundCheckHeight, 1f));
     }
 }
+
--- Assets/Scripts/WorldObjects/Spawner.cs
+++ Assets/Scripts/WorldObjects/Spawner.cs
@@ -2,32 +2,56 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-
+/// <summary>
+/// Enumerates the playable characters in the game to be selected by the player.
+/// </summary>
     public enum PlayableCharacter
     {
         SuperDude,
         SuperGirl
     }
-
+/// <summary>
+/// Manages the spawning of the player characters in the game.
+/// </summary>
 public class Spawner : MonoBehaviour
 {
-    public GameObject superDudePrefab;
-    public GameObject superGirlPrefab;
-    public Transform spawnPoint;
 
-
-    public PlayableCharacter playerChoice; 
+    [SerializeField] private GameObject superDudePrefab;
+    [SerializeField] private GameObject superGirlPrefab;
+    [SerializeField] public Transform spawnPoint;
+     public PlayableCharacter playerChoice;
 
 
     // Start is called before the first frame update
     void Start()
     {
-       
+        SpawnPlayer();
     }
 
-    // Update is called once per frame
-    void Update()
+    /// <summary>
+    /// Handles the spawning of the selected player character.
+    /// </summary>
+    void SpawnPlayer()
     {
-        
+        if (spawnPoint != null && superDudePrefab != null && superGirlPrefab != null)
+        {
+            switch (playerChoice)
+            {
+                case PlayableCharacter.SuperDude:
+                    Instantiate(superDudePrefab, spawnPoint.position, spawnPoint.rotation);
+                    break;
+                case PlayableCharacter.SuperGirl:
+                    Instantiate(superGirlPrefab, spawnPoint.position, spawnPoint.rotation);
+                    break;
+                default:
+                    Debug.LogWarning($"{nameof(Spawner)} > {nameof(SpawnPlayer)}: Unhandled player choice.");
+                    break;
+            }
+        }
+        else
+        {
+            Debug.LogError($"{nameof(Spawner)} > {nameof(SpawnPlayer)}: One or more required components are not assigned.");
+
+        }
     }
 }
